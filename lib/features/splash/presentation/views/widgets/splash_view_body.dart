3447da36f38747ddb8d8bd55f1e7cbfa371e3e8d
medo910 <mahmoud.0420a@gmail.com>
2025-08-14 14:45:10 +0200

    add text to splash view & cahnge theme

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -9,7 +9,11 @@ class SplashViewBody extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssetsData.logo)],
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(height: 4),
+        const Text("Read free Books", textAlign: TextAlign.center),
+      ],
     );
   }
 }

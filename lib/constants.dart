2fba80f4bfbc11f9dc7a557664e7844d5f674e1d
medo910 <mahmoud.0420a@gmail.com>
2025-08-15 12:15:23 +0200

    create styles

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0Xff100B20);
 
 const kTranstionDuration = Duration(milliseconds: 250);
+const kGtSectraFine = 'GtSectraFine';
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -14,11 +14,43 @@ class HomeViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+        children: const [
           CustomAppBar(),
           FeaturedBooksListView(),
-          const SizedBox(height: 50),
-          Text("Best Seller", style: Styles.titleMedium),
+          SizedBox(height: 50),
+          Text("Best Seller", style: Styles.textStyle18),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
+          Column(children: [
+              
+            ],
+          ),
         ],
       ),
     );

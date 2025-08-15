833e747cde458549c2ade6a5ddfbd74a63f0d1e5
medo910 <mahmoud.0420a@gmail.com>
2025-08-15 19:34:35 +0200

    create similar box list view

--- lib/features/home/presentation/views/widgets/custom_appbar_book_details.dart
+++ lib/features/home/presentation/views/widgets/custom_appbar_book_details.dart
@@ -5,15 +5,12 @@ class CustomAppBarBookDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.close)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(onPressed: () {}, icon: Icon(Icons.close)),
+        IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+      ],
     );
   }
 }

51944e122aeeafd106a97f6712d6b15b655c7b8c
medo910 <mahmoud.0420a@gmail.com>
2025-08-15 20:27:37 +0200

    refactor book details view

--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/features/home/presentation/views/widgets/bokk_details_view_body.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_details_view_body.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsView extends StatelessWidget {
---
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -0,0 +1,39 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/core/widgets/custom_button.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_details_section.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_appbar_book_details.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_section.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsViewBody extends StatelessWidget {
+  const BookDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const CustomAppBarBookDetails(),
+                const BookDetailsSection(),
+
+                const Expanded(child: SizedBox(height: 50)),
+                SimilarBooksSection(),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}

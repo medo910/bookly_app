cb58cedc9437cb73402582ad33070c97baaafc8c
medo910 <mahmoud.0420a@gmail.com>
2025-08-15 14:41:41 +0200

    create book details view

---
+++ lib/features/home/presentation/views/widgets/bokk_details_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BokkDetailsViewBody extends StatelessWidget {
+  const BokkDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
